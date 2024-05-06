23534ea9122ee6c66e2b198bd091bde347805a08
Davi Cruz <cruzdavi001@gmail.com>
2024-05-06 16:18:00 -0300

    Exercicio sobre os conceitos iniciais da linguagem Java.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //To Do: conhecer e importar  classe Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //TODO: Obter os valores digitador(pela classe scanner)
+        System.out.println("Digite o numero da conta: ");
+        int numero = scanner.nextInt();
+        System.out.println("Digite a agencia: ");
+        String agencia = scanner.next();
+        System.out.println("Digite o seu nome: ");
+        String nome = scanner.next();
+        System.out.println("Digite o saldo: ");
+        double saldo= scanner.nextDouble();
+
+        //Exibir a mensagem final( da conta criada)
+        System.out.println("Olá "+nome+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque.");
+
+
+        // Fechamos o Scanner para evitar vazamento de recursos:
+        scanner.close();
+
+        
     }
 }
